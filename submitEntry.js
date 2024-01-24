04ca02ebdc91457761b38622cb68fcc39435391a
Thanh Do <113277994+casualprogram@users.noreply.github.com>
2024-01-24 12:55:52 -0500

    Update submitEntry.js

--- submitEntry.js
+++ submitEntry.js
@@ -11,31 +11,7 @@ const fetchCheckOut = require("./functions/fetchCheckOut");
 
 
 async function submitEntry(page, user,password){
-
-    //User input account info
-    // let threadId = prompt('Please enter t-ID : ');
-    // let productId = prompt('Please enter p-ID : ');
-    // let size = prompt('Please enter size : ');
-
-    let threadId = "166c8569-76d7-4b16-98c8-91eb12219a01"
-    let productId = "53d9db35-38f5-52ab-a095-771861ad6c78"
-    let size = "10"
-
-    var checkOutLink = fetchCheckOut(productId, size, threadId)
-    log("link - " + checkOutLink)
-
-    await delay(3000);
-    //go to main page 
-    await page.goto(checkOutLink);
-    // await delay(8000);
-
-    const buttons = await page.$$("div.section-title.ncss-col-sm-5.va-sm-m.pl0-sm.pr0-sm")
-    for (let btn of buttons) {
-       await btn.click()
-    }
-    log('done')
-    await delay(4000);
-
+//maintain
 
 }
 
